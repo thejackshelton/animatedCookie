e967e2918aac9eaf07904ddccd9bc1324663d28d
thejackshelton <jacksheltontx@outlook.com>
2022-06-30 18:05:56 -0500

    Animated Cookie

---
+++ app.js
@@ -0,0 +1,25 @@
+//Creating the animation timeline
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } })
+
+tl.fromTo('.cookie-container', { scale: 0 }, { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5 });
+
+// The <50% starts the animation after the previous line is 50% done.
+tl.fromTo('.cookie', { opacity: 0, x: -50, rotation: '-45deg' }, { opacity: 1, x: 0, rotation: '0deg' }, '<50%')
+
+// The < symbol syncs it with the previous animation
+tl.fromTo('.text', { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, '<')
+
+//Cookie Jump
+tl.fromTo('.cookie', { y: 0, rotation: '0deg' }, { y: -20, rotation: '-20deg', yoyo: true, repeat: -1 })
+tl.fromTo('#crumbs', { y: 0 }, { y: -20, yoyo: true, repeat: -1 }, '<')
+
+//Fading the cookie out
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', { opacity: 0, y: 100, duration: 0.75, ease: "power1.out" })
+})
+
+
+
+// //Using GSAP with animations. First parameter is the element you want to animate
+// gsap.fromTo('.text', { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1 })
